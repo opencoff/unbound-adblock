de5e49214b2bce155a6c3d068909f6e5ece23918
Sudhi Herle <sudhi@herle.net>
2021-10-23 22:08:08 -0700

    Added unbound.py to generate unbound.conf from txt output

---
+++ unbound.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+
+import os, sys
+
+
+header = False
+for ll in sys.stdin:
+    ll = ll.strip()
+    if len(ll) == 0:       continue
+    if ll.startswith('#'):
+        print(ll)
+        continue
+
+    if not header:
+        print("server:")
+        header = True
+
+    print(f'local-zone: "{ll}" static')
+
+
